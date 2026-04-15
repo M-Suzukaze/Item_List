81b2015c800ae736fb3af16433430a2cab798900
M-Suzukaze <82731737+M-Suzukaze@users.noreply.github.com>
2026-04-15 14:03:09 -0400

    Initial Commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module com.example.itemlist {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+
+    opens com.example.itemlist to javafx.fxml;
+    exports com.example.itemlist;
+}
