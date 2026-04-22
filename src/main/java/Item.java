9ed6611c86f3c4a17372d8083a85603f22dddae1
AngeloB-23 <abellina3673@students.mc3.edu>
2026-04-22 13:32:47 -0400

    added the equipment categort to the class and updated the toString

--- src/main/java/Item.java
+++ src/main/java/Item.java
@@ -3,6 +3,7 @@ import java.util.Arrays;
 public class Item {
     public String name;
     public String[] desc;
+    public Equipment_Category equipment_category;
     public Cost cost;
     public Properties[] properties;
     public double weight;
@@ -11,8 +12,9 @@ public class Item {
     public String toString() {
         String s;
         s = this.name + "\n" + Arrays.toString(this.desc) + "\n" +
-                this.cost + "\n" + Arrays.toString(this.properties) +
-                "\n" + this.weight + " pounds \n" + this.url;
+            this.equipment_category + "\n" +
+            this.cost + "\n" + Arrays.toString(this.properties) +
+            "\n" + this.weight + " pounds \n" + this.url;
         return s;
     }
 }
