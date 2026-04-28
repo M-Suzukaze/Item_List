4b62fbec21232d3396580b0684065b0c20d1eb5e
AngeloB-23 <abellina3673@students.mc3.edu>
2026-04-28 15:08:16 -0400

    The item text now displays on the InfoController

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,6 +4,7 @@ module com.example.itemlist {
 
     requires org.controlsfx.controls;
     requires com.google.gson;
+    requires java.net.http;
 
     opens com.example.itemlist to javafx.fxml;
     exports com.example.itemlist;
